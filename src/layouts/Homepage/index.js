7a2b244f6c54dabd501cf004afbff9d5a61bcede
makersden <wearemakersden@gmail.com>
2016-10-30 18:14:11 +0100

    POC animations.

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -1,13 +1,25 @@
-import React, { PropTypes } from "react"
+import React, { PropTypes } from 'react';
+import Typist from 'react-typist';
+import styles from './index.css';
+import envelope from './Envelope.svg';
+import SVGInline from 'react-svg-inline';
 // import enhanceCollection from "phenomic/lib/enhance-collection"
 
-import Page from "../Page"
 // import PagesList from "../../components/PagesList"
 
 // const numberOfLatestPosts = 6
+const cursor = {
+    show: false,
+};
 
-const Homepage = (props// , { collection }
-                 ) => {
+const avgTypingDelay = 40;
+
+
+const showFooter = () => {
+    document.querySelector('footer').classList.remove(styles.hidden);
+};
+
+const Homepage = () => {
   // const latestPosts = enhanceCollection(collection, {
   //   filter: { layout: "Post" },
   //   sort: "date",
@@ -16,9 +28,22 @@ const Homepage = (props// , { collection }
   // .slice(0, numberOfLatestPosts)
 
   return (
-    <Page { ...props }>
-      {/* <PagesList pages={ latestPosts } /> */}
-    </Page>
+  <article className={styles.homepage}>
+    <h1 className={styles.hero}>
+      <Typist cursor={cursor}
+              avgTypingDelay={avgTypingDelay}
+              stdTypingDelay={0}
+              onTypingDone={showFooter}
+          >
+        We are Makers.
+      </Typist>
+    </h1>
+    <footer className={styles.hidden}>
+      <a href="mailto:wearemakersden@gmail.com" className={styles.contact}>
+        <SVGInline svg={envelope} />
+      </a>
+    </footer>
+  </article>
   )
 }
 
