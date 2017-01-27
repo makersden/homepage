d141188c0e4612c5f7811c9fcb511c76f22d03e3
makersden <wearemakersden@gmail.com>
2017-01-27 18:35:51 +0100

    Change star background positioning.

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -14,7 +14,12 @@ const showFooter = () => {
 };
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   scrollTo = (e) => {
+    console.log(e.target.hash, this[e.target.hash.slice(1)], this.scrollContext);
     e.preventDefault();
     smoothscroll(
       this[e.target.hash.slice(1)],
@@ -24,7 +29,16 @@ class Homepage extends Component {
     );
   }
 
+  scrollLink = (id, text) => (
+    <a href={`#${id}`} >{text}</a>
+  )
+
+  setRefById = (e) => {
+    this[e.id] = e;
+  }
+
   render() {
+    const { scrollLink, setRefById } = this;
     return (
       <section className={styles.main}>
         <section className={styles.background}>
@@ -36,26 +50,26 @@ class Homepage extends Component {
         </section>
         <section className={styles.content}>
           <nav className={styles.nav}>
-            <a href="#meet_the_team" onMouseDown={this.scrollTo}>Our Team</a>
-            <a href="#see_our_work">Our Work</a>
+            {scrollLink('crew', 'Our Crew')}
+            {scrollLink('work', 'Our Work')}
             <a href="#home" className={styles.homeLink} onMouseDown={this.scrollTo}>
               MAKERS' DEN
             </a>
-            <a href="">Our Testimonials</a>
-            <a href="">Contact Us</a>
+            {scrollLink('testimonials', 'Our Testimonials')}
+            {scrollLink('contact', 'Contact Us!')}
           </nav>
           <section className={styles.homepageSegments} ref={e => this.scrollContext = e}>
-            <Hero ref={e => this.home = e} />
-            <section id="meet_the_team" ref={(e) => this.meet_the_team = e}>
+            <Hero id="home" />
+            <section id="crew" ref={setRefById}>
               A combined 8278 parsecs of experience
             </section>
-            <section id="work" ref={(e) => this.work = e}>
+            <section id="work" ref={setRefById}>
               4318 projects launched into space
             </section>
-            <section id="testimonials" ref={(e) => this.testimonials = e}>
+            <section id="testimonials" ref={setRefById}>
               "My God it was beautiful." - Some guy
             </section>
-            <section id="contact" ref={(e) => this.contact = e}>
+            <section id="contact" ref={setRefById}>
               In addition to email + phone let's have a chatbox cause that's what everyone does nowadays, and it lowers barrier to contact immensly. I think there's even facebook messenger widgets.
             </section>
           </section>
