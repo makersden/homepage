aca6f9fda3a631b2294588058fb2d932a0a32b36
makersden <wearemakersden@gmail.com>
2017-03-20 22:31:13 +0100

    Add menu highlights.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,27 @@
 import React, { PropTypes } from 'react';
-import classNames from 'classnames';
+import classnames from 'classnames/bind';
 import Svg from 'react-svg-inline';
 
 import styles from './index.scss';
 
-const _link = (currentHash) => (id, text) => (
-  <a href={`/#${id}`} className={classNames(styles.navLink)}>{text}</a>
-);
+const cx  = classnames.bind(styles);
 
-const Header = ({ location: { hash} }, { metadata: { pkg } }) => {
-  const link = _link(hash);
+const _link = (currentHash) => (id, text) => {
+  console.log(id, currentHash);
+  return (
+    <a href={`/#${id}`} className={cx('navLink', { active: id === currentHash })}>{text}</a>
+  );
+};
+
+const Header = ({ location: { hash, pathname } }, { metadata: { pkg } }) => {
+  const hashName = hash.slice(1);
+  const link = _link(hashName);
+  const isHome = pathname === '/' && (!hashName || hashName === 'home');
   return (
     <nav className={styles.nav}>
       {link('team', 'Our Team')}
       {link('work', 'Our Work')}
-      <a href="/#home" className={classNames(styles.homeLink, styles.navLink)}>
+      <a href="/#home" className={cx('homeLink', 'navLink', { active: isHome })}>
         Makers' Den
       </a>
       {link('honors', 'Our Honors')}
