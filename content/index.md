b66f7366a599373bcbe2d1674aaca1d257037f5b
elkorn <helluinster@gmail.com>
2019-04-02 13:13:33 +0200

    Remove old things.

--- content/index.md
+++
@@ -1,5 +0,0 @@
----
-title: Makers' Den
-layout: Homepage
-description: A crack crew of IT veterans, ready to get your software from here to done.
----
--- src/oldLayouts/Homepage/Hero.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./hero.scss";
-
-const Home = () => {
-  return <section id="home" className={styles.hero} />;
-};
-
-export default Home;
